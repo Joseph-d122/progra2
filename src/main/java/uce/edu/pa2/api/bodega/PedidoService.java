9f650620f7d2f68381f405144de3f035e2581a18
Joseph <danycondor851@gmail.com>
2026-04-13 19:31:04 -0500

    Taller 3

--- src/main/java/uce/edu/pa2/api/bodega/PedidoService.java
+++ src/main/java/uce/edu/pa2/api/bodega/PedidoService.java
@@ -6,8 +6,30 @@ import jakarta.inject.Inject;
 @ApplicationScoped
 public class PedidoService {
 
+    //@Inject
+    // private NotificadorMail n1;
+    
+    //DI por Constructor
+    
+    //public PedidoService(NotificadorMail n1) {
+    //    this.n1 = n1;
+
+    // }
+
+    //DI por Atributo
+
+    //@Inject
+    //private NotificadorMail n1;
+    
+    //DI por Metodo Setter
+
+    private NotificadorMail n1;
+
     @Inject
-    private NotificadorMail notificadorMail;
+    public void setN1(NotificadorMail n1) {
+        this.n1 = n1;
+    }
+
     public void registrar(Pedido pedido) {
         System.out.println("Registrando pedido...");
         System.out.println("cliente: "+ pedido.getCliente());
@@ -16,7 +38,7 @@ public class PedidoService {
 
         //Sin inyeccion
         //NotificadorMail n1 = new NotificadorMail();
-        //Con DI por el contenedor se encarga de crear la instancia y inyectarla
-        notificadorMail.enviar(pedido.getCorreo(), "Su ha creado un pedido para ser atendido");
+        n1 = new NotificadorMail(); //Con DI por el contenedor se encarga de crear la instancia y inyectarla
+        n1.enviar(pedido.getCorreo(), "Su ha creado un pedido para ser atendido");
     }
 }
